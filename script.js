b8c38e809b6ab0519a5686a174e6a904722a7c6c
Alexey Tsyabuk <tsyabuk.alexey@gmail.com>
2019-10-20 17:22:39 +0300

    add switch in second block

---
+++ script.js
@@ -0,0 +1,24 @@
+//switching Shipping Addres and Processor Information
+
+const shippingAddress = document.querySelector('.shipping-addres');
+const processorInformation = document.querySelector('.processor-information');
+const shippingAddressIcon = document.querySelector('.shipping-address-icon');
+const processorInfIcon = document.querySelector('.processor-inf-icon');
+
+console.log(shippingAddressIcon,processorInfIcon);
+
+
+shippingAddressIcon.addEventListener('click',()=>{
+    processorInformation.style.display = 'none';
+    shippingAddress.style.display = 'block';
+    shippingAddressIcon.parentNode.classList.add('active-icon');
+    processorInfIcon.parentNode.classList.remove('active-icon');
+});
+
+processorInfIcon.addEventListener('click',()=>{
+    shippingAddress.style.display = 'none';
+    processorInformation.style.display = 'block';
+    shippingAddressIcon.parentNode.classList.remove('active-icon');
+    processorInfIcon.parentNode.classList.add('active-icon');
+});
+
