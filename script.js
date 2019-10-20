c71bae50d40e6600facf207bad39c42f08349ad3
Alexey Tsyabuk <tsyabuk.alexey@gmail.com>
2019-10-20 18:28:38 +0300

    fixed small things

--- script.js
+++ script.js
@@ -5,20 +5,17 @@ const processorInformation = document.querySelector('.processor-information');
 const shippingAddressIcon = document.querySelector('.shipping-address-icon');
 const processorInfIcon = document.querySelector('.processor-inf-icon');
 
-console.log(shippingAddressIcon,processorInfIcon);
 
-
-shippingAddressIcon.addEventListener('click',()=>{
+shippingAddressIcon.addEventListener('click', () => {
     processorInformation.style.display = 'none';
     shippingAddress.style.display = 'block';
     shippingAddressIcon.parentNode.classList.add('active-icon');
     processorInfIcon.parentNode.classList.remove('active-icon');
 });
 
-processorInfIcon.addEventListener('click',()=>{
+processorInfIcon.addEventListener('click', () => {
     shippingAddress.style.display = 'none';
     processorInformation.style.display = 'block';
     shippingAddressIcon.parentNode.classList.remove('active-icon');
     processorInfIcon.parentNode.classList.add('active-icon');
 });
-
